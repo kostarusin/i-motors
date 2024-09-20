fae810ed2ee315cc5bb7b675c89cbac34e71985d
Kostyantyn Tarusin <122443715+kostarusin@users.noreply.github.com>
2024-09-20 13:32:14 +0200

    CEO test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '@/app/globals.css';
 import { roboto } from '@/app/ui/fonts';
+import Head from 'next/head';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Example from './components/Example';
@@ -12,6 +13,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="scroll-smooth">
+      <Head>
+        <title>i-motors</title>
+        <meta
+          name="i-motors"
+          content="mietwerkstatt and autoteile in Recklinghousen"
+        />
+      </Head>
       <body>
         {/* <Example /> */}
         <div>
