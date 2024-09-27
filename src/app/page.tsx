66d94e65f3330ff7c450111682627d3e8b4c0d53
Kostyantyn Tarusin <122443715+kostarusin@users.noreply.github.com>
2024-09-27 15:45:37 +0200

    hero image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Page() {
       <div
         className="relative isolate px-6 lg:px-8 "
         style={{
-          backgroundImage: "url('/contact-3994018.jpg')",
+          backgroundImage: "url('/antique-car-4804725.jpg')",
           backgroundSize: 'cover',
           backgroundPosition: 'center',
           height: '100vh', // Adjust the height as needed
