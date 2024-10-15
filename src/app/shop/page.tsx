154834db24c947ee9e3edeec480e13c8ea0dffb2
Kostyantyn Tarusin <122443715+kostarusin@users.noreply.github.com>
2024-10-15 17:54:50 +0200

    fix

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,28 +1,40 @@
-'use client';
-import CardOfGoods from '../components/CardOfGoods';
-import ShopPageSearch from '../components/ShopPageSearch';
 import { ShoppingCartIcon } from '@heroicons/react/24/outline';
-import Drower from '../components/Drawer';
 
 export default function Shop() {
   return (
-    <main className="px-6 py-12 bg-white">
-      <p className="text-gray-600 font-semibold text-center py-10">
-        Die Seite wird aktuell überarbeitet. Besuchen Sie unser
-        Kleinanzeigen-Profil, um alle Angebote zu entdecken.
-      </p>
-      <a
-        href="https://www.kleinanzeigen.de/s-anzeige/hobbywerkstatt-kfz-mietwerkstatt-hebebuehne-kran-reparatur/2379287664-280-1988"
-        target="_blank"
-        className="text-xl text-green-500 flex justify-center pb-4 hover:text-green-700"
+    <main>
+      <div
+        className="relative isolate px-6 lg:px-8 "
+        style={{
+          backgroundImage: "url('/auto-repair-shop-1954636.jpg')",
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          height: '100vh', // Adjust the height as needed
+        }}
       >
-        <ShoppingCartIcon aria-hidden="true" className="h-6 w-6" />
-        <span className="pl-4">Kleinanzeigen</span>
-      </a>
-
-      {/* <Drower /> */}
-      <div className="pb-12 items-center">{/* <ShopPageSearch /> */}</div>
-      {/* <CardOfGoods /> */}
+        <div className="absolute inset-0 bg-black opacity-60 z-10"></div>
+        <div className="relative z-20 isolate px-6 pt-14 lg:px-8">
+          <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+            <div className="text-center">
+              <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
+                Auoteile
+              </h1>
+              <p className="text-white pt-10">
+                Die Seite wird aktuell überarbeitet. Besuchen Sie unser
+                Kleinanzeigen-Profil, um alle Angebote zu entdecken.
+              </p>
+              <a
+                href="https://www.kleinanzeigen.de/s-anzeige/hobbywerkstatt-kfz-mietwerkstatt-hebebuehne-kran-reparatur/2379287664-280-1988"
+                target="_blank"
+                className="text-xl text-green-500 flex justify-center pt-10 hover:text-green-700"
+              >
+                <ShoppingCartIcon aria-hidden="true" className="h-6 w-6" />
+                <span className="pl-4">Kleinanzeigen</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
